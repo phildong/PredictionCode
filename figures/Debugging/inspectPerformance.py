794e73d3204ad2ff2840833bf00c293d582e849c
Andrew Leifer <leifer@princeton.edu>
2020-01-21 10:31:58 -0500

    Added Details to Inspect Performance: Train R2, Velocity during heatmap

--- figures/Debugging/inspectPerformance.py
+++ figures/Debugging/inspectPerformance.py
@@ -297,6 +297,7 @@ def main():
                         behPred[valid_map] = movingAnalysis[flag][behavior]['output']
 
                     R2_local = calc_R2(beh[valid_map][test], behPred[valid_map][test])
+                    R2_train = calc_R2(beh[valid_map][train], behPred[valid_map][train])
 
                     #Actually Plot
                     axes[ax_cnt].plot(time, beh, label="Measured")
@@ -308,7 +309,8 @@ def main():
                     axes[ax_cnt].title.set_text(pred_type +  ', '
                                                 + title + additional_title_text +
                                                 ' R2 = %.2f' % R2_stored
-                                                + ' R2_loc = %.2f' % R2_local)
+                                                + ' R2_loc = %.2f' % R2_local +
+                                                ' R2_train = %.2f' % R2_train)
                     axes[ax_cnt].legend()
                     axes[ax_cnt].set_xlabel('Time (s)')
                     axes[ax_cnt].set_xlim( [time[valid_map[0]], time[valid_map[-1]]])
@@ -373,7 +375,7 @@ def main():
             plt.suptitle('data[' + key + '][' + idn + ']')
 
 
-            ax = plt.subplot(3,1,1)
+            ax = plt.subplot(4,1,1)
             pos = ax.imshow(I_smooth, aspect='auto',
                             interpolation='none', vmin=np.nanpercentile(I_smooth.flatten(), 0.1), vmax=np.nanpercentile(I_smooth.flatten(), prcntile),
                             extent = [ time[0], time[-1], 0, I_smooth.shape[0] ])
@@ -384,7 +386,7 @@ def main():
             fig.tight_layout(rect=[0, 0.03, 1.1, 0.97])
 
 
-            ax = plt.subplot(3,1,2)
+            ax = plt.subplot(4,1,2)
             pos = ax.imshow(I_smooth_interp_crop_noncontig_wnans, aspect='auto',
                             interpolation='none', vmin=np.nanpercentile(I_smooth_interp_crop_noncontig_wnans,0.1), vmax=np.nanpercentile(I_smooth_interp_crop_noncontig_wnans.flatten(), prcntile),
                             extent = [ time[0], time[-1], 0, I_smooth_interp_crop_noncontig_wnans.shape[0] ])
@@ -394,15 +396,28 @@ def main():
             fig.colorbar(pos, ax=ax)
 
 
-            ax = plt.subplot(3, 1, 3)
+            ax = plt.subplot(4, 1, 3)
             pos = ax.imshow(Iz, aspect='auto',
                             interpolation='none', vmin=-2, vmax=2,
-                            extent = [ time[0], time[-1], 0, Iz.shape[0] ])
+                            extent = [time[0], time[-1], 0, Iz.shape[0] ])
             ax.set_title('Activity  (per-neuron z-scored,  aggressive interpolation, common noise rejected,  Jeffs photobleach correction)')
             ax.set_xlabel('Time (s)')
             ax.set_ylabel('Neuron')
             fig.colorbar(pos, ax=ax)
 
+            beh = dset['BehaviorFull']['AngleVelocity']
+            time = dset['Neurons']['I_Time']
+
+            axbeh = plt.subplot(4, 1, 4)
+            axbeh.plot(time, beh)
+            fig.colorbar(pos, ax=axbeh)
+            axbeh.axhline(linewidth=0.5, color='k')
+            axbeh.set_ylim([-2, 4])
+            axbeh.set_xlim(ax.get_xlim())
+
+            axbeh.set_title('Velocity')
+            axbeh.set_xlabel('Time (s)')
+            axbeh.set_ylabel('Velocity (Body bends)')
 
             prov.stamp(plt.gca(),.9,.15)
 
