9cebc80145de79ae55580e23d8122d793c8c6c8c
Andrew Leifer <leifer@princeton.edu>
2020-02-06 11:42:39 -0500

    got escape response timings

--- figures/Debugging/compareOmegaTurns/extractRelativeTiming.m
+++ figures/Debugging/compareOmegaTurns/extractRelativeTiming.m
@@ -1,24 +1,32 @@
+%Load the spreadsheet
+csvfile = '/home/leifer/workspace/PredictionCode/figures/Debugging/compareOmegaTurns/EscapeResponseTimePoints.csv';
+T = readtable(csvfile,'Format','%s%s%f%u%u%u%u%s','Delimiter',',')
+
+
+
+vlc_bf_FrameRate = T.FPS; %frames / vlc second
+
 % These are the relevant timepoints on the VLC video of the brightfield
-SpM = 60;
-START = 1;
-FINISH = 2;
-vlc_bf(START) = 13 * SpM + 21; %in vlc seconds
-vlc_bf(FINISH) = 13 * SpM + 57; %in vlc seconds
+vlc_bf = [T.StartMin*60 + T.StartSec, T.EndMin * 60 + T.EndSec ]; %in vlc seconds
 
-vlc_bf_FrameRate = 30.0003; %frames / vlc second
-acquired_bf_Framerate = 50; %frames /aquired second
+for k=1:size(vlc_bf,1)
+    bf(k,:) = vlc_bf(k,:).*vlc_bf_FrameRate(k); % brightfield frame number
+    %Get the time alignment between brightfield and hi magnification images
+    
+    % by comparing flashes
+    [bfAll, ~, hiResData] =  tripleFlashAlign(T.BrainScannerFolder{k});
+
+    commonTime(k,:) = [bfAll.frameTime(round(bf(k, START))), bfAll.frameTime(round(bf(k,FINISH)))];
+    
+    %skip repeated time stamps for interpolation otherwise interpolation
+    %will error
+    ind = find(diff(hiResData.frameTime)~=0);
+    volume(k,:) = round(interp1(hiResData.frameTime(ind),hiResData.stackIdx(ind),commonTime(k,:),'linear'));
+
+end
 
 
-bf = vlc_bf*30.0003; % brightfield frame number
 
-
-%Get the time alignment between brightfield and hi magnification images
-% by comparing flashes
-[bfAll, ~, hiResData] =  tripleFlashAlign('/tigress/LEIFER/PanNeuronal/2017/20170610/BrainScanner20170610_105634');
-
-%
-commonTime(START) = bfAll.frameTime(round(bf(START)))
-commonTime(FINISH) = bfAll.frameTime(round(bf(FINISH)))
-
-volume = round(interp1(hiResData.frameTime,hiResData.stackIdx,commonTime,'linear'))
+commonTime
+volume
 
