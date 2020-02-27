eb53af2e53619d4d883de2758dd3795a50de0a87
Ross Dempsey <rossd97@gmail.com>
2020-02-27 13:21:09 -0500

    fix HDF write error

--- prediction/SLM.py
+++ prediction/SLM.py
@@ -16,7 +16,7 @@ import dataHandler as dh
 import userTracker
 
 def train_tree(X, Y):
-    clf = tree.DecisionTreeClassifier(max_depth=1)
+    clf = linear_model.DecisionTreeClassifier(max_depth=1)
     clf = clf.fit(X, np.sign(Y))
     return clf
 
@@ -39,7 +39,7 @@ def optimize_slm(time, Xfull, Yfull, options = None):
     'best_neuron': False,
     'max_depth': 1,
     'alphas': np.logspace(-1.5, 1.5, 7),
-    'l1_ratio': 0.9,
+    'l1_ratios': np.linspace(0, 1, 5),
     'sigma': 14,
     'derivative_weight': 10,
     'cv_fraction': 0.4,
@@ -57,11 +57,11 @@ def optimize_slm(time, Xfull, Yfull, options = None):
         f = lambda X, P: P[1]*X + P[0]
     else:
         f = lambda X, P: np.dot(X.T, P[1:]) + P[0]
-    reg = lambda P: options['l1_ratio']*np.sum(np.abs(P[1:])) + 0.5*(1-options['l1_ratio'])*np.sum(P[1:]*P[1:])
+    reg = lambda P, l1_ratio: l1_ratio*np.sum(np.abs(P[1:])) + 0.5*(1-l1_ratio)*np.sum(P[1:]*P[1:])
 
-    error_plain = lambda P, X, Y, alpha: (1./(4*Y.size*np.var(Y)))*np.sum(np.power(Y-f(X,P),2)) + alpha*reg(P)
+    error_plain = lambda P, X, Y, alpha, l1_ratio: (1./(4*Y.size*np.var(Y)))*np.sum(np.power(Y-f(X,P),2)) + alpha*reg(P, l1_ratio)
     if options['derivative_penalty']:
-        error = lambda P, X, Y, X_deriv, Y_deriv, alpha: error_plain(P, X, Y, alpha) + options['derivative_weight']*(1/(4*Y.size*np.var(Y_deriv)))*np.sum(np.power(Y_deriv-f(X_deriv, P), 2))
+        error = lambda P, X, Y, X_deriv, Y_deriv, alpha, l1_ratio: error_plain(P, X, Y, alpha, l1_ratio) + options['derivative_weight']*(1/(4*Y.size*np.var(Y_deriv)))*np.sum(np.power(Y_deriv-f(X_deriv, P), 2))
     else:
         error = error_plain
 
@@ -72,28 +72,32 @@ def optimize_slm(time, Xfull, Yfull, options = None):
         if not options['best_neuron']:
             if not options['derivative_penalty']:
                 best_alpha = 0
+                best_l1_ratio = 0
                 best_R2 = -np.inf
                 for alpha in options['alphas']:
-                    result = minimize(error, np.zeros(X.shape[0]+1), args=(X_train, Y_train, alpha))
-                    r2 = R2(result.x, f, X_cv, Y_cv) 
-                    if r2 > best_R2:
-                        best_alpha = alpha
-                        best_R2 = r2
+                    for l1_ratio in options['l1_ratios']:
+                        result = minimize(error, np.zeros(X.shape[0]+1), args=(X_train, Y_train, alpha, l1_ratio))
+                        r2 = R2(result.x, f, X_cv, Y_cv) 
+                        if r2 > best_R2:
+                            best_alpha = alpha
+                            best_l1_ratio = l1_ratio
+                            best_R2 = r2
                 
-                result = minimize(error, np.zeros(X.shape[0]+1), args=(X, Y, best_alpha))
+                result = minimize(error, np.zeros(X.shape[0]+1), args=(X, Y, best_alpha, best_l1_ratio))
                 P = result.x
-                return {'weights'   : P[1:],
-                        'offset'    : P[0],
-                        'params'    : P,
-                        'error'     : result.fun,
-                        'alpha'     : best_alpha,
-                        'R2_train'  : R2(P, f, X, Y),
-                        'R2_test'   : R2(P, f, Xtest, Ytest),
-                        'signal'    : Yfull,
-                        'prediction': f(Xfull, P),
-                        'time'      : time,
-                        'train_idx' : train_idx,
-                        'test_idx'  : test_idx
+                return {'weights'        : P[1:],
+                        'intercepts'     : P[0],
+                        'params'         : P,
+                        'error'          : result.fun,
+                        'alpha'          : best_alpha,
+                        'l1_ratio'       : best_l1_ratio,
+                        'score'          : R2(P, f, X, Y),
+                        'scorepredicted' : R2(P, f, Xtest, Ytest),
+                        'signal'         : Yfull,
+                        'output'         : f(Xfull, P),
+                        'time'           : time,
+                        'train_idx'      : train_idx,
+                        'test_idx'       : test_idx
                         }
             
             else:
@@ -103,28 +107,32 @@ def optimize_slm(time, Xfull, Yfull, options = None):
                 Y_train_deriv = gaussian_filter(Y_train, sigma = options['sigma'], order = 1)
 
                 best_alpha = 0
+                best_l1_ratio = 0
                 best_R2 = -np.inf
                 for alpha in options['alphas']:
-                    result = minimize(error, np.zeros(X.shape[0]+1), args=(X_train, Y_train, X_train_deriv, Y_train_deriv, alpha))
-                    r2 = R2(result.x, f, X_cv, Y_cv) 
-                    if r2 > best_R2:
-                        best_alpha = alpha
-                        best_R2 = r2
+                    for l1_ratio in options['l1_ratios']:
+                        result = minimize(error, np.zeros(X.shape[0]+1), args=(X_train, Y_train, X_train_deriv, Y_train_deriv, alpha, l1_ratio))
+                        r2 = R2(result.x, f, X_cv, Y_cv) 
+                        if r2 > best_R2:
+                            best_alpha = alpha
+                            best_l1_ratio = l1_ratio
+                            best_R2 = r2
 
-                result = minimize(error, np.zeros(X.shape[0]+1), args=(X, Y, X_deriv, Y_deriv, best_alpha))
+                result = minimize(error, np.zeros(X.shape[0]+1), args=(X, Y, X_deriv, Y_deriv, best_alpha, best_l1_ratio))
                 P = result.x
-                return {'weights'   : P[1:],
-                        'offset'    : P[0],
-                        'params'    : P,
-                        'error'     : result.fun,
-                        'alpha'     : best_alpha,
-                        'R2_train'  : R2(P, f, X, Y),
-                        'R2_test'   : R2(P, f, Xtest, Ytest),
-                        'signal'    : Yfull,
-                        'prediction': f(Xfull, P),
-                        'time'      : time,
-                        'train_idx' : train_idx,
-                        'test_idx'  : test_idx
+                return {'weights'        : P[1:],
+                        'intercepts'     : P[0],
+                        'params'         : P,
+                        'error'          : result.fun,
+                        'alpha'          : best_alpha,
+                        'l1_ratio'       : options['l1_ratio'],
+                        'score'          : R2(P, f, X, Y),
+                        'scorepredicted' : R2(P, f, Xtest, Ytest),
+                        'signal'         : Yfull,
+                        'output'         : f(Xfull, P),
+                        'time'           : time,
+                        'train_idx'      : train_idx,
+                        'test_idx'       : test_idx
                         }
 
         else:
@@ -136,7 +144,7 @@ def optimize_slm(time, Xfull, Yfull, options = None):
 
                 for i in np.arange(X.shape[0]):
                     neuron = X[i,:]
-                    result = minimize(error, np.zeros(2), args = (neuron, Y, 0))
+                    result = minimize(error, np.zeros(2), args = (neuron, Y, 0, 0))
                     r2 = R2(result.x, f, neuron, Y)
                     if r2 > best_neuron_R2:
                         best_neuron_idx = i
@@ -150,18 +158,19 @@ def optimize_slm(time, Xfull, Yfull, options = None):
                 P[0] = best_neuron_params[0]
                 P[1:] = weights
 
-                return {'weights'   : weights,
-                        'offset'    : best_neuron_params[0],
-                        'params'    : P,
-                        'error'     : best_neuron_error,
-                        'alpha'     : 0,
-                        'R2_train'  : best_neuron_R2,
-                        'R2_test'   : R2(best_neuron_params, f, Xtest[best_neuron_idx,:], Ytest),
-                        'signal'    : Yfull,
-                        'prediction': f(Xfull[best_neuron_idx,:], best_neuron_params),
-                        'time'      : time,
-                        'train_idx' : train_idx,
-                        'test_idx'  : test_idx
+                return {'weights'        : weights,
+                        'intercepts'     : best_neuron_params[0],
+                        'params'         : P,
+                        'error'          : best_neuron_error,
+                        'alpha'          : 0,
+                        'l1_ratio'       : 0,
+                        'score'          : best_neuron_R2,
+                        'scorepredicted' : R2(best_neuron_params, f, Xtest[best_neuron_idx,:], Ytest),
+                        'signal'         : Yfull,
+                        'output'         : f(Xfull[best_neuron_idx,:], best_neuron_params),
+                        'time'           : time,
+                        'train_idx'      : train_idx,
+                        'test_idx'       : test_idx
                         }
             
             else:
@@ -190,18 +199,19 @@ def optimize_slm(time, Xfull, Yfull, options = None):
                 P[0] = best_neuron_params[0]
                 P[1:] = weights
 
-                return {'weights'   : weights,
-                        'offset'    : best_neuron_params[0],
-                        'params'    : P,
-                        'error'     : best_neuron_error,
-                        'alpha'     : 0,
-                        'R2_train'  : best_neuron_R2,
-                        'R2_test'   : R2(best_neuron_params, f, Xtest[best_neuron_idx,:], Ytest),
-                        'signal'    : Yfull,
-                        'prediction': f(Xfull[best_neuron_idx,:], best_neuron_params),
-                        'time'      : time,
-                        'train_idx' : train_idx,
-                        'test_idx'  : test_idx
+                return {'weights'        : weights,
+                        'intercepts'     : best_neuron_params[0],
+                        'params'         : P,
+                        'error'          : best_neuron_error,
+                        'alpha'          : 0,
+                        'l1_ratio'       : 0,
+                        'score'          : best_neuron_R2,
+                        'scorepredicted' : R2(best_neuron_params, f, Xtest[best_neuron_idx,:], Ytest),
+                        'signal'         : Yfull,
+                        'output'         : f(Xfull[best_neuron_idx,:], best_neuron_params),
+                        'time'           : time,
+                        'train_idx'      : train_idx,
+                        'test_idx'       : test_idx
                         }
     
     else:
@@ -220,39 +230,41 @@ def optimize_slm(time, Xfull, Yfull, options = None):
         res2 = optimize_slm(time, X2, Y2, new_options)
 
         prediction_train = np.zeros(Y.size)
-        prediction_train[decision > 0] = np.dot(X1.T, res1['weights']) + res1['offset']
-        prediction_train[decision < 0] = np.dot(X2.T, res2['weights']) + res2['offset']
+        prediction_train[decision > 0] = np.dot(X1.T, res1['weights']) + res1['intercepts']
+        prediction_train[decision < 0] = np.dot(X2.T, res2['weights']) + res2['intercepts']
 
         decision_test = clf.predict(Xtest.T)
         prediction_test = np.zeros(Ytest.size)
-        prediction_test[decision_test > 0] = np.dot(Xtest[:,decision_test>0].T, res1['weights']) + res1['offset']
-        prediction_test[decision_test < 0] = np.dot(Xtest[:,decision_test<0].T, res2['weights']) + res2['offset']
+        prediction_test[decision_test > 0] = np.dot(Xtest[:,decision_test>0].T, res1['weights']) + res1['intercepts']
+        prediction_test[decision_test < 0] = np.dot(Xtest[:,decision_test<0].T, res2['weights']) + res2['intercepts']
 
         decision_full = clf.predict(Xfull.T)
         prediction_full = np.zeros(Yfull.size)
-        prediction_full[decision_full > 0] = np.dot(Xfull[:,decision_full>0].T, res1['weights']) + res1['offset']
-        prediction_full[decision_full < 0] = np.dot(Xfull[:,decision_full<0].T, res2['weights']) + res2['offset']
+        prediction_full[decision_full > 0] = np.dot(Xfull[:,decision_full>0].T, res1['weights']) + res1['intercepts']
+        prediction_full[decision_full < 0] = np.dot(Xfull[:,decision_full<0].T, res2['weights']) + res2['intercepts']
 
         r2_train = 1-np.sum(np.power(Y-prediction_train, 2))/np.sum(np.power(Y-np.mean(Y), 2))
         r2_test = 1-np.sum(np.power(Ytest-prediction_test, 2))/np.sum(np.power(Ytest-np.mean(Ytest), 2))
 
-        return {'clf'        : clf,
-                'weights_pos': res1['weights'],
-                'weights_neg': res2['weights'],
-                'offset_pos' : res1['offset'],
-                'offset_neg' : res2['offset'],
-                'error'      : (Y1.size*res1['error']+Y2.size*res2['error'])/Y.size,
-                'alpha_pos'  : res1['alpha'],
-                'alpha_neg'  : res2['alpha'],
-                'R2_train'   : r2_train,
-                'R2_test'    : r2_test,
-                'signal'     : Yfull,
-                'prediction' : prediction_full,
-                'res_pos'    : res1,
-                'res_neg'    : res2,
-                'time'       : time,
-                'train_idx'  : train_idx,
-                'test_idx'   : test_idx
+        return {'decision'       : decision_full,
+                'weights_pos'    : res1['weights'],
+                'weights_neg'    : res2['weights'],
+                'intercepts_pos' : res1['intercepts'],
+                'intercepts_neg' : res2['intercepts'],
+                'error'          : (Y1.size*res1['error']+Y2.size*res2['error'])/Y.size,
+                'alpha_pos'      : res1['alpha'],
+                'alpha_neg'      : res2['alpha'],
+                'l1_ratio_pos'   : res1['l1_ratio'],
+                'l1_ratio_neg'   : res2['l1_ratio'],
+                'score'          : r2_train,
+                'scorepredicted' : r2_test,
+                'signal'         : Yfull,
+                'output'         : prediction_full,
+                'res_pos'        : res1,
+                'res_neg'        : res2,
+                'time'           : time,
+                'train_idx'      : train_idx,
+                'test_idx'       : test_idx
                 }
 
 if __name__ == '__main__':
@@ -261,7 +273,7 @@ if __name__ == '__main__':
 
     output_data = {}
 
-    for typ_cond in ['AKS297.51_moving']:#, 'AML32_moving']:#, 'AML70_chip', 'AML70_moving', 'AML18_moving']:
+    for typ_cond in ['AKS297.51_moving', 'AML32_moving']:#, 'AML70_chip', 'AML70_moving', 'AML18_moving']:
         path = userTracker.dataPath()
         folder = os.path.join(path, '%s/' % typ_cond)
         dataLog = os.path.join(path,'{0}/{0}_datasets.txt'.format(typ_cond))
@@ -286,9 +298,9 @@ if __name__ == '__main__':
             settings = [(j, k) for j in range(2) for k in range(2)]
             r2s = {}
             for s in settings:
-                res = optimize_slm(time, neurons, velocity, options = {'decision_tree': s[0], 'best_neuron': s[1]})
+                res = optimize_slm(time, neurons, velocity, options = {'decision_tree': s[0], 'best_neuron': s[1], 'l1_ratios': [.9]})
                 r2s[s] = res
-                print(s, res['R2_train'], res['R2_test'])
+                print(s, res['score'], res['scorepredicted'])
             
             output_data[typ_cond+" "+key] = r2s
             
@@ -296,8 +308,8 @@ if __name__ == '__main__':
 
             r2keys = list(r2s.keys())
             labels = list(map(str,r2keys))
-            r2s_train = [r2s[k]['R2_train'] for k in r2keys]
-            r2s_test  = [r2s[k]['R2_test'] for k in r2keys]
+            r2s_train = [r2s[k]['score'] for k in r2keys]
+            r2s_test  = [r2s[k]['scorepredicted'] for k in r2keys]
 
             x = np.arange(len(r2s))  # the label locations
             width = 0.35  # the width of the bars
