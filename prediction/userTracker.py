babe9b9ca042a76f8867140ac257523f7902aad8
Ross Dempsey <rossd97@gmail.com>
2020-05-07 16:51:37 -0400

    ~/PredictionCode

--- prediction/userTracker.py
+++ prediction/userTracker.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """detect which user is using the machine and change the path to the data accordingly."""
-import socket
+import getpass
 
 def dataPath():
 	"""hardcoded or automatic detection of the users' data path."""
@@ -26,7 +26,7 @@ def codePath():
 	elif host=='Ross-PC':
 		path = '/home/srossd/PredictionCode/'
 	elif host=='tigressdata2.princeton.edu':
-		path = '/projects/LEIFER/PanNeuronal/decoding_analysis/worm_data'
+		path = '~/PredictionCode'
 	else:
 		raise Exception('This is not a known host. Edit prediction/userTracker.py to add your hostname and code paths.')
 	return path
