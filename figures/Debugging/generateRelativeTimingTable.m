33e16ffcb0df11e2e591b38b9c99b16a2f951ac4
Andrew Leifer <leifer@princeton.edu>
2020-06-18 10:42:59 -0400

    removed detrius left in realtiveTimingWithVolume code

--- figures/Debugging/generateRelativeTimingTable.m
+++ figures/Debugging/generateRelativeTimingTable.m
@@ -43,41 +43,7 @@ volume = round(interp1(hiResData.frameTime(ind), double(hiResData.stackIdx(ind))
 
 T = array2table([commonTime, bf_frame_indx1', bf_vlc_time_mins', bf_vlc_time_secs', volume], 'VariableNames',{'LabTime_Secs', 'BF_Frame', 'VLC_BF_Mins', 'VLC_BF_Secs', 'volume' });
 
-writetable(T,'realtiveTimingWithVolumes.txt','Delimiter',',') 
+writetable(T,'relativeTimingWithVolumes.txt','Delimiter',',')
 
-
-
-
-
-assert(0,'everything worked')
-
-
-
-% These are the relevant timepoints on the VLC video of the brightfield
-%vlc_bf = [T.StartMin*60 + T.StartSec, T.EndMin * 60 + T.EndSec ]; %in vlc seconds
-
-
-
-
-
-
-for k=1:size(vlc_bf,1)
-    bf(k,:) = vlc_bf(k,:).*vlc_bf_FrameRate(k); % brightfield frame number
-    %Get the time alignment between brightfield and hi magnification images
-    
-    % by comparing flashes
-    [bfAll, ~, hiResData] =  tripleFlashAlign(T.BrainScannerFolder{k});
-
-    commonTime(k,:) = [bfAll.frameTime(round(bf(k, START))), bfAll.frameTime(round(bf(k,FINISH)))];
-    
-    %skip repeated time stamps for interpolation otherwise interpolation
-    %will error
-    ind = find(diff(hiResData.frameTime)~=0);
-    volume(k,:) = round(interp1(hiResData.frameTime(ind),hiResData.stackIdx(ind),commonTime(k,:),'linear'));
-
-end
-
-U=[T,array2table(volume,'VariableNames',{'StartVolume','EndVolume'})];
-writetable(U,'realtiveTimingWithVolumes.txt','Delimiter',',') 
 
 
