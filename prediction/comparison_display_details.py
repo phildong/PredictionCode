b897b798a3742e940e29a9715fc4eb99811002c3
Ross Dempsey <rossd97@gmail.com>
2020-07-13 09:55:47 -0400

    comparison grid plotting

--- prediction/comparison_display_details.py
+++ prediction/comparison_display_details.py
@@ -31,9 +31,9 @@ with open('comparison_results.dat', 'rb') as handle:
 keys = list(data.keys())
 keys.sort()
 
-figtypes = ['bsn_deriv_acc', 'slm_with_derivs_acc']
+figtypes = ['bsn_deriv_acc', 'slm_with_derivs_acc', 'slm_acc']
 
-pdf = matplotlib.backends.backend_pdf.PdfPages(os.path.join(userTracker.codePath(), "comparison_details.pdf"))
+pdf = matplotlib.backends.backend_pdf.PdfPages(os.path.join(userTracker.codePath(), "slm_results.pdf"))
 
 for key in keys:
 
@@ -41,27 +41,8 @@ for key in keys:
     gs = gridspec.GridSpec(len(figtypes)+1, 2, figure=fig, width_ratios=(1, 1))
 
     for row, figtype in enumerate(figtypes):
-        ts = fig.add_subplot(gs[row+1, 0])
         res = data[key][figtype]
-        ts.plot(res['time'], res['signal'], 'k', lw=1)
-        ts.plot(res['time'], res['output'], 'b', lw=1)
-        # w = np.ones(res['time'].size, dtype=bool)
-        # w[:6] = 0
-        # w[-6:] = 0
-        # ts.fill_betweenx(res['output'], np.roll(res['time'], -6), np.roll(res['time'], 6), where=w, color='b', lw=1)
-        ts.set_title(figtype+r' $R^2_\mathrm{test}(\mathrm{velocity})$ = %0.3f, $R^2_\mathrm{test}(\mathrm{acceleration})$ = %0.3f' % (res['scorepredicted'], deriv_r2(data[key][figtype]['signal'], data[key][figtype]['output'], data[key][figtype]['test_idx'])))
-        ts.set_xlabel('Time (s)')
-        ts.set_ylabel('Velocity')
-        ts.fill_between([res['time'][np.min(res['test_idx'])], res['time'][np.max(res['test_idx'])]], np.min(res['signal']), np.max(res['signal']), facecolor='gray', alpha = 0.5)
 
-        sc = fig.add_subplot(gs[row+1, 1])
-        sc.plot(res['signal'][res['train_idx']], res['output'][res['train_idx']], 'go', label = 'Train')
-        sc.plot(res['signal'][res['test_idx']], res['output'][res['test_idx']], 'bo', label = 'Test')
-        sc.plot([min(res['signal']), max(res['signal'])], [min(res['signal']), max(res['signal'])], 'k-.')
-        sc.set_title(figtype+r' $\rho_\mathrm{test}(\mathrm{velocity})$ = %0.3f' % (res['corrpredicted']))
-        sc.set_xlabel('Measured Velocity')
-        sc.set_ylabel('Predicted Velocity')
-        sc.legend()
 
         y = res['signal'][res['test_idx']]
         yhat = res['output'][res['test_idx']]
@@ -79,12 +60,34 @@ for key in keys:
         print("Actual R^2:  ",R2)
         print("Formula R^2: ",res['corrpredicted']**2 - (beta/truesigma)**2 - (alpha+beta**2)**2/((truesigma*predsigma)**2))
 
+
+        ts = fig.add_subplot(gs[row+1, 0])
+        ts.plot(res['time'], res['signal'], 'k', lw=1)
+        ts.plot(res['time'], res['output'], 'b', lw=1)
+        # w = np.ones(res['time'].size, dtype=bool)
+        # w[:6] = 0
+        # w[-6:] = 0
+        # ts.fill_betweenx(res['output'], np.roll(res['time'], -6), np.roll(res['time'], 6), where=w, color='b', lw=1)
+        ts.set_title(figtype+r' $R^2_\mathrm{test}(\mathrm{velocity})$ = %0.3f, $R^2_\mathrm{test}(\mathrm{acceleration})$ = %0.3f' % (R2, deriv_r2(data[key][figtype]['signal'], data[key][figtype]['output'], data[key][figtype]['test_idx'])))
+        ts.set_xlabel('Time (s)')
+        ts.set_ylabel('Velocity')
+        ts.fill_between([res['time'][np.min(res['test_idx'])], res['time'][np.max(res['test_idx'])]], np.min(res['signal']), np.max(res['signal']), facecolor='gray', alpha = 0.5)
+
+        sc = fig.add_subplot(gs[row+1, 1])
+        sc.plot(res['signal'][res['train_idx']], res['output'][res['train_idx']], 'go', label = 'Train', rasterized = True)
+        sc.plot(res['signal'][res['test_idx']], res['output'][res['test_idx']], 'bo', label = 'Test', rasterized = True)
+        sc.plot([min(res['signal']), max(res['signal'])], [min(res['signal']), max(res['signal'])], 'k-.')
+        sc.set_title(figtype+r' $\rho^2_{\mathrm{test},\mathrm{adj}}(\mathrm{velocity})$ = %0.3f' % (res['corrpredicted']**2 - (alpha+beta**2)**2/((truesigma*predsigma)**2)))
+        sc.set_xlabel('Measured Velocity')
+        sc.set_ylabel('Predicted Velocity')
+        sc.legend()
+
     ax = fig.add_subplot(gs[0, :])
 
-    slm_weights = data[key]['slm_with_derivs_acc']['weights']*np.sqrt(data[key]['slm_with_derivs_acc']['variance'])
-    bsn_weights = data[key]['bsn_deriv_acc']['weights']*np.sqrt(data[key]['slm_with_derivs_acc']['variance'])
+    slm_weights = data[key]['slm_acc']['weights']*np.sqrt(data[key]['slm_acc']['variance'])
+    bsn_weights = data[key]['bsn_deriv_acc']['weights'][:len(data[key]['slm_acc']['weights'])]*np.sqrt(data[key]['slm_acc']['variance'])
 
-    n = slm_weights.size/3
+    n = slm_weights.size
 
     xs = np.argsort(slm_weights[:n])
 
@@ -94,17 +97,17 @@ for key in keys:
     ax.plot(scaler.transform(np.array([slm_weights[xs]]).T)+4, 'k.', label = 'SLM')
     ax.plot(scaler.transform(np.zeros(xs.size).reshape(1,-1)).T+4, 'b', linestyle='dashed')
 
-    scaler.fit(np.array([slm_weights[xs+n]]).T)
+    # scaler.fit(np.array([slm_weights[xs+n]]).T)
 
-    ax.plot(scaler.transform(np.array([slm_weights[xs+n]]).T)+2, 'k.')
-    ax.plot(scaler.transform(np.zeros(xs.size).reshape(1,-1)).T+2, 'b', linestyle='dashed')
+    # ax.plot(scaler.transform(np.array([slm_weights[xs+n]]).T)+2, 'k.')
+    # ax.plot(scaler.transform(np.zeros(xs.size).reshape(1,-1)).T+2, 'b', linestyle='dashed')
 
-    scaler.fit(np.array([slm_weights[xs+2*n]]).T)
+    # scaler.fit(np.array([slm_weights[xs+2*n]]).T)
 
-    ax.plot(scaler.transform(np.array([slm_weights[xs+2*n]]).T), 'k.')
-    ax.plot(scaler.transform(np.zeros(xs.size).reshape(1,-1)).T, 'b', linestyle='dashed')
+    # ax.plot(scaler.transform(np.array([slm_weights[xs+2*n]]).T), 'k.')
+    # ax.plot(scaler.transform(np.zeros(xs.size).reshape(1,-1)).T, 'b', linestyle='dashed')
 
-    for i in range(3):
+    for i in range(1):
         bn = np.array([bsn_weights[xs+i*n]]).T
         bn_idx = np.nonzero(bn)[0]
         if bn_idx.size > 0:
@@ -113,11 +116,11 @@ for key in keys:
 
     ax.axhline(1.5)
     ax.axhline(3.5)
-    ax.set_ylim(-0.5, 5.5)
+    ax.set_ylim(3.5, 5.5)
 
     ax.text(-2, 3.8, '$F$', fontsize=16)
-    ax.text(-2, 1.8, r'$\left.\frac{dF}{dt}\right|_+$', fontsize=16)
-    ax.text(-2, -0.2, r'$\left.\frac{dF}{dt}\right|_-$', fontsize=16)
+    # ax.text(-2, 1.8, r'$\left.\frac{dF}{dt}\right.$', fontsize=16)
+    # ax.text(-2, -0.2, r'$\left.\frac{dF}{dt}\right|_-$', fontsize=16)
 
     ax.set_xticks(np.arange(n))
     ax.set_xticklabels(xs)
