345cb2fa07954fca2c72cbe2cd6ceb1e6e64e390
Andrew Leifer <leifer@princeton.edu>
2020-10-05 06:21:22 -0400

    add df/dt to tuning of highly weighted neurons

--- figures/2020_subpanels/tuning_of_highly_weighted_neurons.py
+++ figures/2020_subpanels/tuning_of_highly_weighted_neurons.py
@@ -23,14 +23,9 @@ excludeInterval = {'BrainScanner20200309_145927': [[50, 60], [215, 225]],
                    'BrainScanner20200310_141211': [[200, 210], [240, 250]]}
 
 def take_deriv(neurons):
-    nan_zero = np.copy(neurons)
-    nan_zero[np.isnan(neurons)] = 0
-    nan_zero_filtered = gaussian_filter(nan_zero, order = 1, sigma = (0, 7))
-    flat = 0*neurons.copy()+1
-    flat[np.isnan(neurons)] = 0
-    flat_filtered = gaussian_filter(flat, order = 0, sigma = (0, 7))
-    deriv = nan_zero_filtered/flat_filtered
-    return deriv
+    from prediction.Classifier import rectified_derivative
+    _, _, nderiv = rectified_derivative(neurons)
+    return nderiv
 
 neuron_data = {}
 deriv_neuron_data = {}
@@ -86,41 +81,51 @@ highly_weighted_neurons = np.flipud(np.argsort(np.abs(slm_weights_raw)))
 highly_weighted_neurons_deriv = np.flipud(np.argsort(np.abs(slm_weights_raw_deriv)))
 num_neurons=len(highly_weighted_neurons)
 
-for rank in np.arange(num_neurons):
-    neuron = highly_weighted_neurons[rank]
-    weight= slm_weights_raw[neuron]
-    activity = neuron_data[key][neuron]
+for type in ['F', 'dF/dt']:
+    for rank in np.arange(num_neurons):
+        if type == 'F':
+            neuron = highly_weighted_neurons[rank]
+            weight = slm_weights_raw[neuron]
+            activity = neuron_data[key][neuron]
+        elif type == 'dF/dt':
+            neuron = highly_weighted_neurons_deriv[rank]
+            weight = slm_weights_raw_deriv[neuron]
+            activity = deriv_neuron_data[key][neuron]
+        else:
+            assert False
 
-    #Calculate bins for box plot and split data up into subarrays based on bin
-    nbins = 10
-    plus_epsilon = 1.00001
-    bin_edges = np.linspace(np.nanmin(vel_data[key]) * plus_epsilon, np.nanmax(vel_data[key]) * plus_epsilon, nbins)
-    binwidth = np.diff(bin_edges)
-    assigned_bin = np.digitize(vel_data[key], bin_edges)
-    activity_bin = [None] * (len(bin_edges) - 1)  # note the activity has to be lists, and there should be 1 less because the bins are edges
-    for k, each in enumerate(np.unique(assigned_bin)):
-        activity_bin[k] = activity[np.argwhere(assigned_bin == each)[:, 0]]
 
-    fig1 = plt.figure(constrained_layout=True, figsize=[15, 8])
-    fig1.suptitle(key + ' Neuron: %d, Weight Rank: %d, Weight = %.4f' % (neuron, rank, weight))
-    gs = gridspec.GridSpec(ncols=4, nrows=2, figure=fig1)
 
-    #Generate box plot
-    f1_ax1 = fig1.add_subplot(gs[0, 0], xlabel=behavior, ylabel='Activity (F)')
-    f1_ax1.plot(vel_data[key], activity, 'o', alpha=.05)
-    boxprops = dict(linewidth=1.5)
-    medianprops = dict(linewidth=3.5)
-    labels = [''] * len(activity_bin)
-    f1_ax1.boxplot(activity_bin, positions=bin_edges[:-1] + binwidth / 2, widths=binwidth * .9, boxprops=boxprops,
-                medianprops=medianprops, labels=labels, manage_xticks=False)
-    plt.locator_params(nbins=4)
+        #Calculate bins for box plot and split data up into subarrays based on bin
+        nbins = 10
+        plus_epsilon = 1.00001
+        bin_edges = np.linspace(np.nanmin(vel_data[key]) * plus_epsilon, np.nanmax(vel_data[key]) * plus_epsilon, nbins)
+        binwidth = np.diff(bin_edges)
+        assigned_bin = np.digitize(vel_data[key], bin_edges)
+        activity_bin = [None] * (len(bin_edges) - 1)  # note the activity has to be lists, and there should be 1 less because the bins are edges
+        for k, each in enumerate(np.unique(assigned_bin)):
+            activity_bin[k] = activity[np.argwhere(assigned_bin == each)[:, 0]]
 
-    f1_ax2 = fig1.add_subplot(gs[0,1:], xlabel='time (s)', ylabel='Activity')
-    f1_ax2.plot(time_data[key], activity)
-    f1_ax3 = fig1.add_subplot(gs[1,1:], xlabel='time (s)', ylabel=behavior)
-    f1_ax3.plot(time_data[key], data[key]['slm_with_derivs']['output'], 'k')
-    prov.stamp(f1_ax3, .55, .35, __file__ + '\n' + pickled_data)
-    pdf.savefig(fig1)
+        fig1 = plt.figure(constrained_layout=True, figsize=[15, 8])
+        fig1.suptitle(key + '  ' + type + ' Neuron: %d, Weight Rank: %d, Weight = %.4f' % (neuron, rank, weight))
+        gs = gridspec.GridSpec(ncols=4, nrows=2, figure=fig1)
+
+        #Generate box plot
+        f1_ax1 = fig1.add_subplot(gs[0, 0], xlabel=behavior, ylabel='Activity (' + type + ')')
+        f1_ax1.plot(vel_data[key], activity, 'o', alpha=.05)
+        boxprops = dict(linewidth=1.5)
+        medianprops = dict(linewidth=3.5)
+        labels = [''] * len(activity_bin)
+        f1_ax1.boxplot(activity_bin, positions=bin_edges[:-1] + binwidth / 2, widths=binwidth * .9, boxprops=boxprops,
+                    medianprops=medianprops, labels=labels, manage_xticks=False)
+        plt.locator_params(nbins=4)
+
+        f1_ax2 = fig1.add_subplot(gs[0,1:], xlabel='time (s)', ylabel='Activity')
+        f1_ax2.plot(time_data[key], activity)
+        f1_ax3 = fig1.add_subplot(gs[1,1:], xlabel='time (s)', ylabel=behavior)
+        f1_ax3.plot(time_data[key], data[key]['slm_with_derivs']['output'], 'k')
+        prov.stamp(f1_ax3, .55, .35, __file__ + '\n' + pickled_data)
+        pdf.savefig(fig1)
 
 pdf.close()
 print("wrote " + outfilename)
