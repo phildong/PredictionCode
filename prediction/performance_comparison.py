e8ef24ea35435ce320fa34a0c60de48b24a80978
Andrew Leifer <leifer@princeton.edu>
2020-11-09 08:13:51 -0500

    Begin switching to rho2_adj1

--- prediction/performance_comparison.py
+++ prediction/performance_comparison.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pickle
-behavior= 'velocity'
-outfile = 'performance_comparison_deriv_rho2_' + behavior + '_l10.pdf'
+behavior= 'curvature'
+outfile = 'performance_comparison_deriv_rho21_' + behavior + '_l10.pdf'
 
 pickled_data = '/projects/LEIFER/PanNeuronal/decoding_analysis/analysis/comparison_results_' + behavior + '_l10.dat'
 with open(pickled_data, 'rb') as handle:
@@ -20,7 +20,7 @@ offset = .7
 ax.set_xticks([0-delta, 1+delta, offset+2-delta, offset+3+delta])
 ax.set_xticklabels(['BSN', 'Population', 'BSN (GFP control)', 'Population (GFP control)'])
 ax.set_ylabel(r'$\rho^2_{\mathrm{adj},\mathrm{test}}$', fontsize=20)
-ax.set_ylim(-0.8, 1)
+ax.set_ylim(-0.5, 1)
 ax.set_yticks([ -.5,  0,  .5,  1])
 ax.tick_params(axis='both', which='major', labelsize=19)
 
@@ -36,7 +36,7 @@ def calc_rho2_adj2(data, key, type='slm_with_derivs'):
 
     truesigma = np.std(y)
     predsigma = np.std(yhat)
-    rho2_adj = (res['corrpredicted'] ** 2 - alpha ** 2 / (truesigma * predsigma) ** 2)
+    rho2_adj = (res['corrpredicted'] ** 2 - (alpha + beta**2) ** 2 / (truesigma * predsigma) ** 2)
     print(key + ': %.2f' % rho2_adj)
     return rho2_adj
 
@@ -55,7 +55,8 @@ for k, key in enumerate(data.keys()): #Comparison line plot
 ax.boxplot([bsn_rho, slm_rho], positions=[0-delta, 1+delta],
            manage_xticks=False, medianprops=medianprops,
            boxprops=boxprops, capprops=capprops, whiskerprops=whiskerprops)
-
+print("Total GCAMP datasets:")
+print(k+1)
 
 bsn_rho_g = np.zeros(len(dataGFP.keys()))
 slm_rho_g = np.zeros(len(dataGFP.keys()))
@@ -70,8 +71,35 @@ if SHOW_GFP:
     ax.boxplot([bsn_rho_g, slm_rho_g], positions=offset+np.array([2-delta, 3+delta]),
                manage_xticks=False, medianprops=medianprops,
                boxprops=boxprops, capprops=capprops, whiskerprops=whiskerprops)
+    print("Total GFP datasets:")
+    print(k+1)
 
 #import prediction.provenance as prov
 #prov.stamp(ax,.55,.35,__file__)
 ax.set_title(outfile)
 fig.savefig(outfile)
+print(outfile)
+
+from scipy.stats import wilcoxon
+d = slm_rho - bsn_rho
+w, p = wilcoxon(d)
+print("P-value for two-sided wilcoxon signed-ranked test on GCAMP")
+print(d)
+print(p)
+
+from scipy.stats import wilcoxon
+d = slm_rho_g - bsn_rho_g
+w, p = wilcoxon(d)
+print("P-value for two-sided wilcoxon signed-ranked test on GFP")
+print(p)
+
+import scipy
+print(scipy.version.full_version)
+
+print(" Note I'm not using this version of wilcoxon signed rank tests it uses an approximationt hat is only valid for large numbers")
+print(" Instead I use a more recent version that allows me to pass the mode='exact' flag ")
+
+from scipy import stats
+t, p = stats.ttest_ind(slm_rho, slm_rho_g, equal_var=False)
+print('Welchs unequal variance t-test from population GCAMP to population GFP:')
+print(p)
