7ab1af72eeadfc81f7aa7e9f2f497a83f0ee4b59
Andrew Leifer <leifer@princeton.edu>
2021-04-27 15:11:52 -0400

    Record the G that went into motion corrected I in dataHandler.py Also set heatmaps to use scale 99th percentile(|G-<G>|)
    Justificaiton: the difference between the dynamic range of the raw green channel signal and teh motion corrected signal is indicative of the extent to which motion correction is taking place.

--- prediction/dataHandler.py
+++ prediction/dataHandler.py
@@ -525,7 +525,6 @@ def loadData(folder, dataPars, ew=1, cutVolume = None):
 
 
         if debug:
-
             nplots=2
             chosen = np.round(np.random.rand(nplots)*R.shape[0]).astype(int)
             import matplotlib.pyplot as plt
@@ -554,10 +553,12 @@ def loadData(folder, dataPars, ew=1, cutVolume = None):
     #Reject noise common to both Red and Green Channels using ICA
     #I = decorrelateNeuronsICA(R, G)
     I = decorrelateNeuronsLinear(R, G)
-    print("After ICA",np.nanmean(I))
+    print("After motion correction",np.nanmean(I))
 
-    #Apply Gaussian Smoothing (and interpolate for free)
-    I_smooth_interp =np.array([gauss_filterNaN(line,dataPars['windowGCamp']) for line in I])
+    #Apply Gaussian Smoothing (and interpolate for free) #Keep the raw green around so we can use it to set heatmap values laters
+    I_smooth_interp = np.array([gauss_filterNaN(line,dataPars['windowGCamp']) for line in I])
+    G_smooth_interp = np.array([gauss_filterNaN(line,dataPars['windowGCamp']) for line in G])
+
 
     print("After smoothing and interpolating", np.nanmean(I_smooth_interp))
 
@@ -566,6 +567,8 @@ def loadData(folder, dataPars, ew=1, cutVolume = None):
     #Reinstate the NaNs
     I_smooth = np.copy(I_smooth_interp)
     I_smooth[np.isnan(I)]=np.nan
+    G_smooth = np.copy(G_smooth_interp)
+    G_smooth[np.isnan(G)] = np.nan
 
 
     #Get the  preferred order  of the neruons,
@@ -617,18 +620,21 @@ def loadData(folder, dataPars, ew=1, cutVolume = None):
     valid_map_identity = valid_map[valid_map > cutVolume]
 
     I_smooth_interp_crop_noncontig_data = np.copy(I_smooth_interp[:, valid_map_data])
+    G_smooth_interp_crop_noncontig_data = np.copy(G_smooth_interp[:, valid_map_data])
     I_smooth_interp_crop_noncontig_identity = np.copy(I_smooth_interp[:, valid_map_identity])
+
+
 
     print(np.mean(I_smooth_interp_crop_noncontig_data))
     #<DEPRECATED>
 
     # load neural data
-    R = np.array(data['rPhotoCorr'])[:, :len(np.array(data['hasPointsTime']))]
-    G = np.array(data['gPhotoCorr'])[:, :len(np.array(data['hasPointsTime']))]
+    RedMatlab = np.array(data['rPhotoCorr'])[:, :len(np.array(data['hasPointsTime']))]
+    GreenMatlab = np.array(data['gPhotoCorr'])[:, :len(np.array(data['hasPointsTime']))]
     #
     Ratio = np.array(data['Ratio2'])[:, :len(np.array(data['hasPointsTime']))]
 
-    Y, dR, GS, RS, RM = preprocessNeuralData(R, G, dataPars)
+    Y, dR, GS, RS, RM = preprocessNeuralData(RedMatlab, GreenMatlab, dataPars)
 
     Ratio = np.array(data['Ratio2'])[:, :len(np.array(data['hasPointsTime']))]
 
@@ -671,9 +677,6 @@ def loadData(folder, dataPars, ew=1, cutVolume = None):
     nonNan_identities = nonNan[nonNan > cutVolume]
 
     #</deprecated>
-
-
-
 
     #Setup time axis
     time = np.squeeze(data['hasPointsTime'])
@@ -738,14 +741,19 @@ def loadData(folder, dataPars, ew=1, cutVolume = None):
 
     # Andys improved photobleaching correction, mean- and variance-preserved variables
 
-    dataDict['Neurons']['I'] = I[order][:,idx_data] # common noise rejected, w/ NaNs, mean- and var-preserved, outlier removed, photobleach corrected
+    dataDict['Neurons']['I'] = I[order][:,idx_data] # linear motion corrected (mean 0, variance preserved) , w/ NaNs,  outlier removed, photobleach corrected
     dataDict['Neurons']['I_Time'] = time[idx_data] #corresponding time axis
-    dataDict['Neurons']['I_smooth'] = I_smooth[order][:,idx_data] # SMOOTHED common noise rejected, has nans, mean- and var-preserved, outlier removed, photobleach corrected
-    dataDict['Neurons']['I_smooth_interp'] = I_smooth_interp[order][:,idx_data] # interpolated, nans added back in, SMOOTHED common noise rejected, mean- and var-preserved, outlier removed, photobleach corrected
-    dataDict['Neurons']['R'] = R[order] #outlier removed, photobleach corrected
-    dataDict['Neurons']['G'] = G[order] #outlier removed, photobleach corrected
+    dataDict['Neurons']['I_smooth'] = I_smooth[order][:,idx_data] # SMOOTHED , has nans, linear motion corrected (mean 0, variance preserved) ,, outlier removed, photobleach corrected
+    dataDict['Neurons']['I_smooth_interp'] = I_smooth_interp[order][:,idx_data] # interpolated, nans added back in, SMOOTHED,linear motion corrected (mean 0, variance preserved) , outlier removed, photobleach corrected
+    dataDict['Neurons']['G'] = G[order][:,idx_data] # outlier removed, photobleach corrected
+    dataDict['Neurons']['G_smooth'] = G_smooth[order][:,idx_data] # SMOOTHED has nans,  outlier removed, photobleach corrected
+    dataDict['Neurons']['G_smooth_interp'] = G_smooth_interp[order][:,idx_data] # interpolated, nans added back in, SMOOTHED, outlier removed, photobleach corrected
+
+    dataDict['Neurons']['RedMatlab'] = RedMatlab[order] #outlier removed, photobleach corrected
+    dataDict['Neurons']['GreenMatlab'] = GreenMatlab[order] #outlier removed, photobleach corrected
 
     dataDict['Neurons']['I_smooth_interp_crop_noncontig'] = I_smooth_interp_crop_noncontig_data[order] # interpolated, SMOOTHED common noise rejected, mean- and var-preserved, outlier removed, photobleach corrected, note strings of nans have been removed such that the DeltaT between elements is no longer constant
+    dataDict['Neurons']['G_smooth_interp_crop_noncontig'] = G_smooth_interp_crop_noncontig_data[order] # interpolated, SMOOTHED, linear motion correction (mean 0 var preserved), , outlier removed, photobleach corrected, note strings of nans have been removed such that the DeltaT between elements is no longer constant
     dataDict['Neurons']['I_Time_crop_noncontig'] = time[valid_map_data]  # corresponding time axis
     dataDict['Neurons']['I_valid_map']=valid_map_data
 
