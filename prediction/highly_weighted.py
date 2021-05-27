4154c5ed696e94f194c9e435c96a04f2f71f62c3
Ross Dempsey <rossd97@gmail.com>
2021-05-27 12:14:56 -0400

    update highly_weighted for new pickle format

--- prediction/highly_weighted.py
+++ prediction/highly_weighted.py
@@ -3,7 +3,7 @@ from matplotlib import animation
 import numpy as np
 from scipy.ndimage import gaussian_filter
 import pickle
-datafolder = '/projects/LEIFER/PanNeuronal/decoding_analysis/analysis/'
+datafolder = '/home/sdempsey/'
 
 def rho_adj(y, yhat):
     truemean = np.mean(y)
@@ -35,22 +35,18 @@ def rectified_derivative(neurons):
 
     return deriv_pos, deriv_neg, deriv
 
-pickled_data = datafolder+'comparison_results_velocity_l10.dat'
+pickled_data = datafolder+'new_comparison.dat'
 with open(pickled_data, 'rb') as handle:
-    vel_data = pickle.load(handle)#, encoding = 'bytes')
+    data = pickle.load(handle)#, encoding = 'bytes')
 
-pickled_data = datafolder+'comparison_results_curvature_l10.dat'
-with open(pickled_data, 'rb') as handle:
-    curv_data = pickle.load(handle)#, encoding = 'bytes')
-
-with open(datafolder+'neuron_data.dat', 'rb') as f:
+with open(datafolder+'neuron_data_bothmc_nb.dat', 'rb') as f:
     neuron_data = pickle.load(f)#, encoding = 'bytes')
 
 nKeys=11
 fig, ax = plt.subplots(3, 4, figsize = (25, 15))
-keys = sorted(vel_data.keys(), key = lambda x: -vel_data[x][b'slm_with_derivs'][b'scorespredicted'][1])
-rho2_adj_vel = np.array([vel_data[keys[x]]['slm_with_derivs']['scorespredicted'][1] for x in np.arange(nKeys)])
-rho2_adj_curv = np.array([curv_data[keys[x]]['slm_with_derivs']['scorespredicted'][1] for x in np.arange(nKeys)])
+keys = sorted(vel_data.keys(), key = lambda x: -data[x]['velocity'][False]['scorespredicted'][1])
+rho2_adj_vel = np.array([vel_data[keys[x]]['velocity'][False]['scorespredicted'][1] for x in np.arange(nKeys)])
+rho2_adj_curv = np.array([curv_data[keys[x]]['curvature'][False]['scorespredicted'][1] for x in np.arange(nKeys)])
 
 #preallocate arrays for plotting later
 n_impact_vel, n_impact_curv,  n_impact_overlap= np.zeros(nKeys), np.zeros(nKeys), np.zeros(nKeys)
@@ -62,8 +58,8 @@ for ii, dataset in enumerate(keys[:nKeys]):
     row = ii // 4
     col = ii % 4
 
-    vel_res = vel_data[dataset][b'slm_with_derivs']   
-    curv_res = curv_data[dataset][b'slm_with_derivs']   
+    vel_res = data[dataset]['velocity'][False]
+    curv_res = data[dataset]['curvature'][False]
 
     neurons_unn = neuron_data[dataset][b'neurons']
     _, _, nderiv = rectified_derivative(neurons_unn)
