a05b3b3b9cae8e58f1c743de5a92e2aaf4cc3d88
phildong <35715936+phildong@users.noreply.github.com>
2023-07-31 08:38:48 -0400

    implemented full and reduced prc model

---
+++ 99.plot_pcr_comparison.py
@@ -0,0 +1,51 @@
+# %% imports and definition
+import os
+import pickle as pkl
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+DPATH_GCAMP = "./intermediate/gcamp_linear_models.dat"
+DPATH_GFP = "./intermediate/gfp_linear_models.dat"
+FIG_PATH = "./figs/pcr_comparison"
+os.makedirs(FIG_PATH, exist_ok=True)
+
+# %% load and construct data
+with open(DPATH_GCAMP, "rb") as pklf:
+    data_gcamp = pkl.load(pklf)
+with open(DPATH_GFP, "rb") as pklf:
+    data_gfp = pkl.load(pklf)
+
+result_df = []
+for grp, ds in {"GCAMP": data_gcamp, "GFP": data_gfp}.items():
+    for dat_key, dat in ds.items():
+        for behav_var, behav_dat in dat.items():
+            for mod, mod_dat in behav_dat.items():
+                if mod == True:
+                    mod = "BSN"
+                elif mod == False:
+                    mod = "Population"
+                res = pd.Series(
+                    {
+                        "group": grp,
+                        "dat_key": dat_key,
+                        "behav_var": behav_var,
+                        "model": mod,
+                        "R2ms_train": mod_dat["R2ms_train"],
+                        "R2ms_test": mod_dat["R2ms_test"],
+                    }
+                )
+                result_df.append(res)
+result_df = pd.concat(result_df, axis="columns", ignore_index=True).T.astype(
+    {"R2ms_train": float, "R2ms_test": float}
+)
+
+# %% plot results
+for yvar in ["R2ms_train", "R2ms_test"]:
+    g = sns.FacetGrid(result_df, row="behav_var", col="group", margin_titles=True)
+    g.map_dataframe(sns.boxplot, x="model", y=yvar, width=0.4, color="white")
+    g.map_dataframe(sns.lineplot, x="model", y=yvar, hue="dat_key")
+    g.fig.tight_layout()
+    g.fig.savefig(os.path.join(FIG_PATH, "{}.svg".format(yvar)), bbox_inches="tight")
--- prediction/train_linear_model.py
+++ prediction/train_linear_model.py
@@ -2,6 +2,7 @@ import pickle
 
 import numpy as np
 from scipy.ndimage import gaussian_filter1d
+from sklearn.decomposition import PCA
 
 from .models import linear
 
@@ -12,18 +13,64 @@ for gtype in ["gcamp", "gfp"]:
     results = {}
 
     for key in list(data.keys()):
+        # book-keeping
         print(("Running " + key))
         time = data[key]["time"]
         neurons = data[key]["neurons"]
         nderiv = data[key]["neuron_derivatives"]
         velocity = data[key]["velocity"]
         curvature = data[key]["curvature"]
-
-        neurons_and_derivs = np.vstack((neurons, nderiv))
-
         results[key] = {}
         results[key]["velocity"] = {}
         results[key]["curvature"] = {}
+        # generate inputs
+        pca = PCA()
+        neuron_pca = pca.fit_transform(neurons.T).T
+        neuron_pca_rd = neuron_pca[3:, :]
+        nderiv_pca = pca.fit_transform(nderiv.T).T
+        nderiv_pca_rd = nderiv_pca[3:, :]
+        nnd_pca = np.vstack((neuron_pca, nderiv_pca))
+        nnd_pca_rd = np.vstack((neuron_pca_rd, nderiv_pca_rd))
+        neurons_and_derivs = np.vstack((neurons, nderiv))
+        # run pcr
+        for pcr in ["full", "reduced"]:
+            if pcr == "full":
+                Xin = nnd_pca
+            else:
+                Xin = nnd_pca_rd
+            results[key]["velocity"][pcr] = linear.optimize(
+                time,
+                Xin,
+                velocity,
+                options_override={
+                    "l1_ratios": [0],
+                    "parallelize": False,
+                    "best_neuron": False,
+                },
+            )
+            results[key]["curvature"][pcr] = linear.optimize(
+                time,
+                Xin,
+                curvature,
+                options_override={
+                    "l1_ratios": [0],
+                    "parallelize": False,
+                    "best_neuron": False,
+                },
+            )
+            print(
+                (
+                    "\tVelocity (%s)  R^2_ms = %0.2f"
+                    % (pcr, results[key]["velocity"][pcr]["R2ms_test"])
+                )
+            )
+            print(
+                (
+                    "\tCurvature (%s) R^2_ms = %0.2f"
+                    % (pcr, results[key]["curvature"][pcr]["R2ms_test"])
+                )
+            )
+
         for bsn in [True, False]:
             results[key]["velocity"][bsn] = linear.optimize(
                 time,
